b80d370376f14ce25fe16a5a947d1ebc4cb83871
Abdirahman Nur <corsium.spongea@gmail.com>
2024-10-27 16:13:48 -0400

    Database connection

--- app/api/index.js
+++ app/api/index.js
@@ -1,30 +1,31 @@
+import mysql from 'mysql';
+import cors from 'cors';
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "sql5.freemysqlhosting.net",
+    user: "sql5740788",
+    password: "lMRMCaTiRZ",
+    database: "users"
+});
 
 app.get('/', (req,res) => {
     res.json({ message: "ok" });
 });
 
-app.post('/route/:id', (req,res) => {
-    const { id } = req.params;
-
-
-    res.json({
-        message: `We recieved your ${id}`
-    })
-})
-
-app.post('/route/:id', (req,res) => {
-    const { id } = req.params;
-
-
-    res.json({
-        message: `We recieved your ${id}`
-    })
-})
+app.post('/select', (req,res) => {
+    const query = "SELECT * FROM users";
+    db.query(query, (err,data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    });
+});
 
 app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}`); 
