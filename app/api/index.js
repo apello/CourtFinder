07072087e58a863c09d02f011d90081ece511870
Abdirahman Nur <corsium.spongea@gmail.com>
2024-10-27 23:03:44 -0400

    Added mysql database connection

--- app/api/index.js
+++ app/api/index.js
@@ -1,6 +1,6 @@
-import mysql from 'mysql';
 import cors from 'cors';
 import express from 'express';
+import pool from './db.js'; // Mysql pool connection
 
 const app = express();
 const port = 3000;
@@ -8,23 +8,16 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: "sql5.freemysqlhosting.net",
-    user: "sql5740788",
-    password: "lMRMCaTiRZ",
-    database: "users"
-});
-
-app.get('/', (req,res) => {
-    res.json({ message: "ok" });
-});
-
 app.post('/select', (req,res) => {
     const query = "SELECT * FROM users";
-    db.query(query, (err,data) => {
+    pool.getConnection((err,db) => {
         if(err) return res.json(err);
-        return res.json(data);
-    });
+
+        db.query(query, (err,data) => {
+            if(err) return res.json(err);
+            return res.json(data);
+        });
+    })   
 });
 
 app.listen(port, () => {
