02ca0a327e0733b117fb388cc84311932ceb4d1d
Abdirahman Nur <corsium.spongea@gmail.com>
2024-10-30 23:14:33 -0400

    Added React functionality to auth forms

--- app/src/auth/login.js
+++ app/src/auth/login.js
@@ -1,23 +1,46 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+    // Set state variables for username, password
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // stop from from reloading page
+    
+        alert(`Your username is ${username} and your password is ${password}!`);
+    };
+
     return (
         <>
             <Link to="/">Go home</Link>
 
             <h2>Login form:</h2>
-            <form method="post">
+            <form method="post" onSubmit={(e) => handleSubmit(e)}>
                 <div style={{ display: "flex", flexDirection: "column", gap: 5, border: "1px solid black", padding: 10 }}>
                     <label for="username">Username:</label>
-                    <input type="text" name="username" placeholder="Enter your username:" />
+                    {/* update the username everytime the input is changed, i.e., typed into */}
+                    <input 
+                        type="text" 
+                        name="username" 
+                        placeholder="Enter your username:" 
+                        maxlength="50"
+                        onChange={(e) => setUsername(e.target.value)} />
 
                     <label for="password">Password:</label>
-                    <input type="password" name="password" placeholder="Enter your password:" />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Enter your password:"
+                        maxlength="50"
+                        onChange={(e) => setPassword(e.target.value)} />
 
                     <input type="submit" />
                 </div>
             </form>
 
+            <br />
             <Link to="/signup">Don't have an account? Sign up</Link>
         </>
     );
--- app/src/auth/signup.js
+++ app/src/auth/signup.js
@@ -1,33 +1,73 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Signup = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // stop from from reloading page
+    
+        alert(`This is what you entered: 
+            ${name}, ${username}, ${email}, ${password}, ${confirm}
+        `);
+    };
+
+
     return (
         <>
             <Link to="/">Go home</Link>
 
             <h2>Sign up</h2>
 
-            <form method="post">
+            <form method="post" onSubmit={(e) => handleSubmit(e)}>
                 <div style={{ display: "flex", flexDirection: "column", gap: 5, border: "1px solid black", padding: 10 }}>
                     <label for="name">Name:</label>
-                    <input type="text" name="name" placeholder="Enter your name:" />
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Enter your name:" 
+                        maxlength="50"
+                        onChange={(e) => setName(e.target.value)} />
 
                     <label for="email">Email:</label>
-                    <input type="email" name="email" placeholder="Enter your email:" />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Enter your email:" 
+                        maxlength="50"
+                        onChange={(e) => setEmail(e.target.value)} />
                     
                     <label for="username">Username:</label>
-                    <input type="text" name="username" placeholder="Enter your username:" />
+                    <input 
+                        type="text" 
+                        name="username" 
+                        placeholder="Enter your username:" 
+                        maxlength="50"
+                        onChange={(e) => setUsername(e.target.value)} />
 
                     <label for="password">Password:</label>
-                    <input type="password" name="password" placeholder="Enter your password:" />
-
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Enter your password:" 
+                        maxlength="50"
+                        onChange={(e) => setPassword(e.target.value)} />
                     <label for="confirm">Confirm Password:</label>
-                    <input type="password" name="confirm" placeholder="Confirm your password:" />
-
+                    <input 
+                        type="password" 
+                        name="confirm" 
+                        placeholder="Confirm your password:" 
+                        maxlength="50"
+                        onChange={(e) => setConfirm(e.target.value)} />
                     <input type="submit" />
                 </div>
             </form>
 
+            <br />
             <Link to="/login">Already have an account? Log in</Link>
         </>
     );
